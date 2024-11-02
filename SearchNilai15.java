5808517f73fcd99032fb83790498c440a7e8d1f7
Rifky <rifkypraditya1025@gmail.com>
2024-11-02 11:48:51 +0700

    modifikasi file SearchNilai15.java

--- SearchNilai15.java
+++ SearchNilai15.java
@@ -1,19 +1,33 @@
+import java.util.Scanner;
 public class SearchNilai15 {
   public static void main(String[] args) {
-    int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-    int key = 90;
-    int hasil = 0;
-
+    Scanner userInput = new Scanner(System.in);
+    int jmlNilai, key, hasil;
+    hasil = 0;
+    // mengambil input dari user
+    System.out.print("Masukkan banyaknya nilai yang akan diinput : ");
+    jmlNilai = userInput.nextInt();
+    int[] arrNilai = new int[jmlNilai];  // membuat array
+    // mengisi nilai pada elemen array
     for (int i = 0; i < arrNilai.length; i++) {
-      if (key == arrNilai[i]) {
-        hasil = i;
-        break;
-      }
+      System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+      arrNilai[i] = userInput.nextInt();
     }
-
-    System.out.println();
-    System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-    System.out.println();
-    
+    // input nilai yg ingin dicari
+    System.out.print("Masukkan nilai yang ingin dicari : ");
+    key = userInput.nextInt();
+    // mencari nilai
+    for (int i = 0; i < arrNilai.length; i++) {
+      if (arrNilai[i] == key) {
+        hasil = i + 1;
+        break;
+      } 
+    }
+    // menampilkan nilai ketemu atau tidak ketemu
+    if (hasil != 0) {
+      System.out.println("\nNilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + hasil + "\n");
+    } else {
+      System.out.println("\nNilai yang dicari tidak ditemukan\n");
+    }
   }
 }
